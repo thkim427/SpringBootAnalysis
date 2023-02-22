e9d65f9541f0054af61f2241c016a0f4726a5df8
taehyunkim427 <thkim427@naver.com>
2023-02-22 20:18:42 +0900

    [20230222] [Containerless] create servlet container

--- src/main/java/com/springboot/analysis/AnalysisApplication.java
+++ src/main/java/com/springboot/analysis/AnalysisApplication.java
@@ -3,12 +3,19 @@ package com.springboot.analysis;
 //import org.springframework.boot.SpringApplication;
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+
 //@SpringBootApplication
 public class AnalysisApplication {
 
     public static void main(String[] args) {
-        System.out.println("Containerless Standalone Application");
         //SpringApplication.run(AnalysisApplication.class, args);
+        //System.out.println("Containerless Standalone Application");
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // tomcat servlet web server 만들어 주는 클래스
+        WebServer webServer = serverFactory.getWebServer(); // 웹서버 생성
+        webServer.start(); // tomcat 시작
     }
 
 }
