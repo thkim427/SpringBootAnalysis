1ac7cf65baf6a8230239d6796121005019415174
taehyunkim427 <thkim427@naver.com>
2023-02-23 23:03:13 +0900

    [Containerless] utilize controller

--- src/main/java/com/springboot/analysis/AnalysisApplication.java
+++ src/main/java/com/springboot/analysis/AnalysisApplication.java
@@ -27,19 +27,26 @@ public class AnalysisApplication {
         //System.out.println("Containerless Standalone Application");
         TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory(); // tomcat servlet web server 만들어 주는 클래스
         WebServer webServer = serverFactory.getWebServer(servletContext -> { // servlet container에 servlet을 등록, @FunctionalInterface이므로 lambda식으로 변형 가능
+            AnalysisController analysisController = new AnalysisController();
+
             servletContext.addServlet(/*"hello"*/"Front-Controller", new HttpServlet() { // servlet interface 자리에 공통적인 코드를 미리 구현해놓고 상속해서 사용하는 adapter 클래스를 override
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
                     // front-controller를 통해 인증, 보안, 다국어, 공통기능... 수행
-                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {  // servlet mapping
                         //super.service(req, resp);
                         String name = req.getParameter("name"); // 요청의 매개변수 name을 받음
+
+                        String ret = analysisController.hello(name);
+
                         // 응답구성 : 상태코드, 상태값, 컨텐츠타입, 헤더, 바디
-                        resp.setStatus(200);
+                        //resp.setStatus(200);
+                        resp.setStatus(HttpStatus.OK.value()); // 오타의 위험, 관리의 편의로 spring에서 제공하는 enum으로 변경
                         //resp.setHeader("Content-Type", "text/plain");
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE); // 오타의 위험, 관리의 편의로 spring에서 제공하는 enum으로 변경
-                        resp.getWriter().println("Hello " + name); // Body에 문자열 응답을 주기 위한 writer
+                        //resp.getWriter().println("Hello " + name); // Body에 문자열 응답을 주기 위한 writer
+                        resp.getWriter().println(ret); // web 응답을 만드는데에 controller 활용
                     }
                     else if (req.getRequestURI().equals("/user")) {
 
@@ -49,7 +56,7 @@ public class AnalysisApplication {
                     }
 
                 }
-            }).addMapping("/hello"); // servlet 컨테이너가 요청을 맵핑해줄 url 필요
+            }).addMapping("/*"); // servlet 컨테이너가 요청을 맵핑해줄 url 필요
         }); // servlet container 생성
         webServer.start(); // tomcat 시작
     }
--- src/main/java/com/springboot/analysis/AnalysisController.java
+++ src/main/java/com/springboot/analysis/AnalysisController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // hello 요청을 받으면 응답을 보내는 controller
-@RestController // html을 통채로 return하지 않고 api 요청에 대한 응답을 body에 특정한 type으로 encoding해서 보내주는 controller
+// @RestController // html을 통채로 return하지 않고 api 요청에 대한 응답을 body에 특정한 type으로 encoding해서 보내주는 controller
 public class AnalysisController {
-    @GetMapping("/hello") // Get으로 된 web 요청, url path가 hello로 시작하는 url만 받는다.
+    // @GetMapping("/hello") // Get으로 된 web 요청, url path가 hello로 시작하는 url만 받는다.
     public String hello(String name) {
         return "Hello " + name;
     }
