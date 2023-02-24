4bc1bafbe94c286749ab0a9239bd108e80e03525
taehyunkim427 <thkim427@naver.com>
2023-02-25 00:44:14 +0900

    [Annotation] meta annotation

--- src/test/java/com/springboot/analysis/AnalysisServiceTest.java
+++ src/test/java/com/springboot/analysis/AnalysisServiceTest.java
@@ -3,8 +3,25 @@ package com.springboot.analysis;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest {
+}
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD}) // meta annotation으로 사용 가능하도록
+@Test
+@interface UnitTest {
+}
+
 public class AnalysisServiceTest {
-    @Test
+    @FastUnitTest
     void simpleAnalysisService() {
         SimpleAnalysisService analysisService = new SimpleAnalysisService();
 
