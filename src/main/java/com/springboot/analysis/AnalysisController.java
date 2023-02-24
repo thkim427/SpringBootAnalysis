2aa1171dbb6296ddd470f81657d9d39344d58996
taehyunkim427 <thkim427@naver.com>
2023-02-24 09:18:35 +0900

    [Spring Container] utilize annotation mapping

--- src/main/java/com/springboot/analysis/AnalysisController.java
+++ src/main/java/com/springboot/analysis/AnalysisController.java
@@ -1,10 +1,14 @@
 // web의 요청을 받아서 결과를 주는 컴포넌트
 package com.springboot.analysis;
 
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Objects;
 
 // hello 요청을 받으면 응답을 보내는 controller
-// @RestController // html을 통채로 return하지 않고 api 요청에 대한 응답을 body에 특정한 type으로 encoding해서 보내주는 controller
+// @RestController // html을 통채로 return하지 않고 api 요청에 대한 응답을 body에 특정한 type으로 encoding해서 보내주는 controller, Dispatcher Servlet과는 직접 관계는 없음
+//@RequestMapping // Dispatcher Servlet이 Class level의 정보를 찾은 이후 Method level 정보를 추가
+@RequestMapping("/hello")
 public class AnalysisController {
     // @GetMapping("/hello") // Get으로 된 web 요청, url path가 hello로 시작하는 url만 받는다.
 
@@ -14,7 +18,11 @@ public class AnalysisController {
         this.analysisService = analysisService; // 생성시 parameter로 service를 받아서 멤버 변수에 저장
     }
 
-    public String hello(String name) {
+    //@RequestMapping(value = "/hello", method = RequestMethod.GET) // GetMapping 만들어지기 전에 사용되던 방식
+    // @GetMapping("/hello") // Dispatcher Servlet이 Web 요청을 처리할 수 있는 Controller로 판단. 매핑 테이블을 만든 후 담당 할 bean object와 method 지정. Get 메소드, url hello
+    @GetMapping // Class level의 매핑 정보와 함께 Method level의 정보를 제공. hello url의 Post, Delete... 제외한 Get 요청만 받는다.
+    @ResponseBody // RestController로 지정 시 defalut 모든 method는 ResponseBody를 붙인것으로 인식
+    public String hello(String name) { // String을 return시 Dispatcher Servlet은 기본적으로 view(html template ex)jsp, timeleaf...) 이름을 찾는다.
         //SimpleAnalysisService analysisService = new SimpleAnalysisService(); // 직접 service 컴포넌트를 생성해서 사용하는 방식
         // assembler (spring container)에서 bean을 주입
         // - controller 생성 시 생성자 parameter로 service object를 주입
