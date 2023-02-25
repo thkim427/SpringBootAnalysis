5a32f8ffc3cd08ce48d25a3965b4edffd6af045e
thkim427 <thkim427@naver.com>
2023-02-25 17:43:58 +0900

    [Annotation] utilize import selector

--- src/main/java/com/springboot/config/EnableMyAutoConfiguration.java
+++ src/main/java/com/springboot/config/EnableMyAutoConfiguration.java
@@ -13,6 +13,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE) // TYPE : class interface enum... meta annotation으로 사용 가능
-@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class}) // @Component 클래스를 지정해주면 구성정보로 직접 추가 가능
+//@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class}) // @Component 클래스를 지정해주면 구성정보로 직접 추가 가능
+@Import(MyAutoConfigImportSelector.class) // import select를 구현한 method의 결과 config class loading
 public @interface EnableMyAutoConfiguration {
 }
