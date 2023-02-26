0643d9755161e9060ac94b13569368340f532bf1
thkim427 <thkim427@naver.com>
2023-02-26 21:50:28 +0900

    [AutoConfiguration] add jetty dependency configuration

--- src/main/java/com/springboot/config/autoconfig/JettyWebServerConfig.java
+++ src/main/java/com/springboot/config/autoconfig/JettyWebServerConfig.java
@@ -1,16 +1,16 @@
 package com.springboot.config.autoconfig;
 
 import com.springboot.config.MyAutoConfiguration;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
-    @Bean("tomcatWebServerFactory") // User 구성 정보에서 제외필요. 패키지를 옮기면 ComponentScan 대상 제외됨
+public class JettyWebServerConfig {
+    @Bean("jettyWebServerFactory") // @Bean을 통해 factory method통해 만들때 기본 이름은 메소드명 servletWebServerFactory. 이름 충돌 방지를 위해 지정
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 }
