4c83c95aa4ca33ccd371764d454f780fb16ac121
taehyunkim427 <thkim427@naver.com>
2023-03-01 13:51:18 +0900

    [Auto Configuration] conditional on missing bean

--- src/main/java/com/springboot/analysis/WebServerConfiguration.java
+++ src/main/java/com/springboot/analysis/WebServerConfiguration.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Component;
 
 @Configuration(proxyBeanMethods = false) // 관례적으로 bean 사이의 상호 의존 관계 주입이 필요 없다면 false
 public class WebServerConfiguration {
+
+    // 유저 구성정보가 항상 자동 구성정보보다 우선되기때문에 @ConditionalOnMissingBean 안전.
+    // 자동 구성정보간 생성 순서는 주의해야함
     @Bean
     ServletWebServerFactory customWebServerFactory() {
         UndertowServletWebServerFactory serverFactory = new UndertowServletWebServerFactory();
